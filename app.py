af50c476dc78696329ed472b60507ea6a1b4121d
shubhpatil28 <shubhampatil282487@gmail.com>
2026-05-27 19:17:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def chat():
         }
 
         payload = {
-            "model": "openai/gpt-3.5-turbo",
+    "model": "meta-llama/llama-3-8b-instruct:free",
             "messages": [
                 {
                     "role": "user",
